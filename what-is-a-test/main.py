d671207969e4cade6697b2b93810cdbfca553788
Dylan PETERANO <dylan.peterano.work@gmail.com>
2025-07-15 18:05:13 -1000

    Exercice de test

---
+++ what-is-a-test/main.py
@@ -0,0 +1,4 @@
+def somme(a, b):
+    return a + b
+def is_even(a, b):
+    return a == b
---
+++ what-is-a-test/test.py
@@ -0,0 +1,6 @@
+from main import *
+
+def test_somme():
+    assert somme(5,5) == 10
+def test_is_even():
+    assert is_even(5, 5) == True
